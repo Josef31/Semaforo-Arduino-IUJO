73d6f46155bf6c51832dcff45a2da4ebc164c5b4
Sofia181006 <sofiarod397@gmail.com>
2026-08-02 10:04:21 -0400

    configuracion y mapeo de pines de hardware corregido

---
+++ config.h
@@ -0,0 +1,21 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <Arduino.h>
+
+// --- EJE ESTE-OESTE (Pines 2 al 6) ---
+const int peatVerdeEO = 2;
+const int peatRojoEO = 3;
+const int vehVerdeEO = 4;
+const int vehAmarilloEO = 5;
+const int vehRojoEO = 6;
+
+// --- EJE NORTE-SUR (Pines 7 al 11) ---
+const int vehVerdeNS = 7;
+const int vehAmarilloNS = 8;
+const int vehRojoNS = 9;
+const int peatRojoNS = 10;
+const int peatVerdeNS = 11;
+
+#endif
