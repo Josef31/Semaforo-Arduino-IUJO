cc62cac07e1974666fd68d27a7fe57b67e5de903
Josef31 <josef310798@gmail.com>
2026-08-02 09:05:34 -0400

    Agregando el main

---
+++ main.ino
@@ -0,0 +1,16 @@
+// main.ino
+#include "config.h"
+#include "fases.h"
+
+void setup() {
+  for (int pin = 2; pin <= 11; pin++) {
+    pinMode(pin, OUTPUT);
+  }
+}
+
+void loop() {
+  fase1_ViaLibreNS();
+  fase2_TransicionNS();
+  fase3_ViaLibreEO();
+  fase4_TransicionEO();
+}
